69952981e7733a1deffbe268918327e08f51d9be
Dima Snisarenko <snisarenkodima@gmail.com>
2026-01-20 10:32:33 +0100

    generate cards

--- mobile-app/src/GenerateCards/GenerateCardsModal.tsx
+++ mobile-app/src/GenerateCards/GenerateCardsModal.tsx
@@ -7,18 +7,10 @@ import {
   UnitOfSpeechGenerationMessage,
 } from '@vocably/model';
 import { last } from 'lodash-es';
-import React, { FC, useState } from 'react';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import { Surface, useTheme } from 'react-native-paper';
+import React, { FC, useRef, useState } from 'react';
+import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native-keyboard-controller';
+import { useTheme } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ChatTextInput } from '../Chat/ChatTextInput';
 import { Message } from '../Chat/Message';
@@ -79,6 +71,7 @@ export const GenerateCardsModal: FC<Props> = ({ route, navigation }) => {
   const translationPresetState = useTranslationPreset();
   const theme = useTheme();
   const [inputText, setInputText] = useState('');
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const deck = useLanguageDeck({
     language:
@@ -215,159 +208,159 @@ export const GenerateCardsModal: FC<Props> = ({ route, navigation }) => {
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={Platform.OS === 'android' ? 64 : 94}
     >
       <ScreenLayout
         content={
-          <ScrollView
-            contentContainerStyle={{
-              flexGrow: 1,
-              paddingTop: insets.top + padding,
+          <View
+            style={{
+              flex: 1,
+              marginBottom: 8,
+              marginLeft: insets.left + 16,
+              marginRight: insets.right + 16,
+              borderRadius: 16,
+              overflow: 'hidden',
             }}
           >
-            <TouchableWithoutFeedback
-              onPress={Keyboard.dismiss}
-              accessible={false}
+            <ScrollView
+              keyboardShouldPersistTaps={'handled'}
+              contentContainerStyle={{
+                flexGrow: 1,
+                gap: 8,
+              }}
+              ref={scrollViewRef}
             >
               <View
                 style={{
-                  flex: 1,
+                  alignSelf: 'flex-start',
+                  backgroundColor: theme.colors.elevation.level5,
+                  borderRadius: 16,
+                  paddingHorizontal: 16,
+                  paddingVertical: 16,
+                  marginTop: 24,
                   gap: 8,
+                  marginLeft: insets.left + padding,
+                  marginRight: insets.right + padding,
                 }}
               >
-                <View
-                  style={{
-                    alignSelf: 'flex-start',
-                    backgroundColor: theme.colors.elevation.level5,
-                    borderRadius: 16,
-                    paddingHorizontal: 16,
-                    paddingVertical: 16,
-                    marginTop: 24,
-                    gap: 8,
-                    marginLeft: insets.left + padding,
-                    marginRight: insets.right + padding,
-                  }}
-                >
+                <Text style={styles.infoText}>
+                  Welcome to the card generator. Type what you need, and Vocably
+                  will generate cards for you.
+                </Text>
+                <Text style={styles.infoText}>
+                  This is an experimental feature. It works pretty poorly, but
+                  I'm improving it.{' '}
+                  <Text
+                    style={linkStyle}
+                    onPress={() => navigation.navigate('Feedback')}
+                  >
+                    Let me know
+                  </Text>{' '}
+                  if you find any bugs or have any suggestions.
+                </Text>
+                <Text style={styles.infoText}>Some examples to try:</Text>
+
+                {languagesWithIrregularVerbs.includes(
+                  translationPresetState.preset.sourceLanguage
+                ) && (
                   <Text style={styles.infoText}>
-                    Welcome to the card generator. Type what you need, and
-                    Vocably will generate cards for you.
-                  </Text>
-                  <Text style={styles.infoText}>
-                    This is an experimental feature. It works pretty poorly, but
-                    I'm improving it.{' '}
+                    -{' '}
                     <Text
                       style={linkStyle}
-                      onPress={() => navigation.navigate('Feedback')}
+                      onPress={runExample('irregular verbs')}
                     >
-                      Let me know
-                    </Text>{' '}
-                    if you find any bugs or have any suggestions.
-                  </Text>
-                  <Text style={styles.infoText}>Some examples to try:</Text>
-
-                  {languagesWithIrregularVerbs.includes(
-                    translationPresetState.preset.sourceLanguage
-                  ) && (
-                    <Text style={styles.infoText}>
-                      -{' '}
-                      <Text
-                        style={linkStyle}
-                        onPress={runExample('irregular verbs')}
-                      >
-                        irregular verbs
-                      </Text>
+                      irregular verbs
                     </Text>
+                  </Text>
+                )}
+                <Text style={styles.infoText} onPress={runExample('animals')}>
+                  - <Text style={linkStyle}>animals</Text>
+                </Text>
+                <Text
+                  style={styles.infoText}
+                  onPress={runExample('popular idioms')}
+                >
+                  - <Text style={linkStyle}>popular idioms</Text>
+                </Text>
+              </View>
+              {messages.map((message, index) => (
+                <View key={index}>
+                  {message.role === 'user' && (
+                    <View
+                      style={{
+                        paddingLeft: insets.left + padding,
+                        paddingRight: insets.left + padding,
+                      }}
+                    >
+                      <Message direction="toAi" message={message.message} />
+                    </View>
                   )}
-                  <Text style={styles.infoText} onPress={runExample('animals')}>
-                    - <Text style={linkStyle}>animals</Text>
-                  </Text>
-                  <Text
-                    style={styles.infoText}
-                    onPress={runExample('popular idioms')}
-                  >
-                    - <Text style={linkStyle}>popular idioms</Text>
-                  </Text>
-                </View>
-                {messages.map((message, index) => (
-                  <View key={index}>
-                    {message.role === 'user' && (
-                      <View
-                        style={{
-                          paddingLeft: insets.left + padding,
-                          paddingRight: insets.left + padding,
-                        }}
-                      >
-                        <Message direction="toAi" message={message.message} />
-                      </View>
-                    )}
-                    {message.role === 'assistant' && (
-                      <AnalyzeResult
-                        cardsLimit={'unlimited'}
-                        leftInset={insets.left}
-                        rightInset={insets.right}
-                        style={{
-                          flex: 1,
-                          width: '100%',
-                          marginRight: 8,
-                        }}
-                        analysis={{
-                          // @ts-ignore
-                          items: message.items,
+                  {message.role === 'assistant' && (
+                    <AnalyzeResult
+                      cardsLimit={'unlimited'}
+                      leftInset={insets.left}
+                      rightInset={insets.right}
+                      style={{
+                        flex: 1,
+                        width: '100%',
+                        marginRight: 8,
+                      }}
+                      analysis={{
+                        // @ts-ignore
+                        items: message.items,
+                        sourceLanguage: translationPresetState.preset
+                          .sourceLanguage as GoogleLanguage,
+                        targetLanguage: translationPresetState.preset
+                          .sourceLanguage as GoogleLanguage,
+                        source: '',
+                        translation: {
                           sourceLanguage: translationPresetState.preset
                             .sourceLanguage as GoogleLanguage,
                           targetLanguage: translationPresetState.preset
                             .sourceLanguage as GoogleLanguage,
                           source: '',
-                          translation: {
-                            sourceLanguage: translationPresetState.preset
-                              .sourceLanguage as GoogleLanguage,
-                            targetLanguage: translationPresetState.preset
-                              .sourceLanguage as GoogleLanguage,
-                            source: '',
-                            target: '',
-                          },
-                        }}
-                        cards={deck.deck.cards}
-                        onAdd={(card) => {
-                          return onAdd(card);
-                        }}
-                        onRemove={onRemove}
-                        onTagsChange={onTagsChange}
-                        deck={deck}
-                        isSharedLookup={false}
-                      />
-                    )}
-                  </View>
-                ))}
-                {thinkingStage !== 'done' && (
-                  <View
-                    style={{
-                      paddingLeft: insets.left + padding,
-                      paddingRight: insets.left + padding,
-                    }}
-                  >
-                    <Thinking
-                      message={
-                        thinkingStage === 'generating-list'
-                          ? 'Generating the list of items...'
-                          : 'Creating cards...'
-                      }
+                          target: '',
+                        },
+                      }}
+                      cards={deck.deck.cards}
+                      onAdd={(card) => {
+                        return onAdd(card);
+                      }}
+                      onRemove={onRemove}
+                      onTagsChange={onTagsChange}
+                      deck={deck}
+                      isSharedLookup={false}
                     />
-                  </View>
-                )}
-              </View>
-            </TouchableWithoutFeedback>
-          </ScrollView>
+                  )}
+                </View>
+              ))}
+              {thinkingStage !== 'done' && (
+                <View
+                  style={{
+                    paddingLeft: insets.left + padding,
+                    paddingRight: insets.left + padding,
+                  }}
+                >
+                  <Thinking
+                    message={
+                      thinkingStage === 'generating-list'
+                        ? 'Generating the list of items...'
+                        : 'Creating cards...'
+                    }
+                  />
+                </View>
+              )}
+            </ScrollView>
+          </View>
         }
         footer={
-          <Surface
-            elevation={0}
+          <View
             style={{
-              paddingLeft: insets.left + padding,
-              paddingRight: insets.right + padding,
-              paddingTop: padding,
-              paddingBottom: padding,
+              paddingBottom: insets.bottom + 16,
+              paddingLeft: insets.left + 16,
+              paddingRight: insets.right + 16,
               gap: 8,
             }}
           >
@@ -384,8 +377,9 @@ export const GenerateCardsModal: FC<Props> = ({ route, navigation }) => {
               onSubmit={send}
               disabled={thinkingStage !== 'done'}
               multiline={true}
+              autoFocus={true}
             />
-          </Surface>
+          </View>
         }
       />
     </KeyboardAvoidingView>
