b54d4e1dae99a84a63d3e027bc3e552478076386
Dima Snisarenko <snisarenkodima@gmail.com>
2026-02-03 11:29:22 +0100

    feat(backend): public feedback

---
+++ packages/public-backend/src/lambdas/user-feedback/index.spec.ts
@@ -0,0 +1,35 @@
+import { inspect } from '@vocably/node-sulna';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { userFeedback } from './index';
+
+// @ts-ignore
+let mockEvent: APIGatewayProxyEvent = {
+  // @ts-ignore
+  requestContext: {
+    authorizer: {
+      claims: {
+        'cognito:username': 'test-user',
+      },
+    },
+  },
+};
+
+process.env.FEEDBACK_NOTIFICATION_PREFIX = '[TEST] ';
+
+describe('integration check for userFeedback lambda', () => {
+  if (process.env.TEST_SKIP_SPEC === 'true') {
+    it('skip spec testing', () => {});
+    return;
+  }
+
+  it('execute translate lambda', async () => {
+    mockEvent.body = JSON.stringify({
+      feedback: `multi
+line
+feedback`,
+    });
+    const result = await userFeedback(mockEvent);
+    console.log(inspect({ result }));
+    expect(result.statusCode).toEqual(200);
+  });
+});
