719b0caa6250ae091305b28fc0e94be7482b4054
Dima Snisarenko <snisarenkodima@gmail.com>
2026-02-10 09:53:00 +0100

    ci: kill and resurrect dev apis

---
+++ scripts/apis-kill.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ./platform
+terraform init -backend-config=./env-${env_name}.remote
+terraform workspace select ${env_name}
+terraform destroy \
+  -target=aws_apigatewayv2_api_mapping.public_api \
+  -target=aws_api_gateway_base_path_mapping.deployment \
+  -var-file="env-${env_name}.tfvars" \
+  -auto-approve
