3ddd24ebf53034a8c3f57943170a00ea3602f857
Dima Snisarenko <snisarenkodima@gmail.com>
2026-04-22 12:24:06 +0200

    feat(grammar-checker): skeleton loader

--- packages/extension-content-ui/src/components/grammar-fixer/grammar-fixer.tsx
+++ packages/extension-content-ui/src/components/grammar-fixer/grammar-fixer.tsx
@@ -41,6 +41,7 @@ export class VocablyFixGrammar {
 
   private textElement: HTMLTextAreaElement;
   private contextElement: HTMLInputElement;
+  private resultElement: HTMLDivElement;
   private unsubLocale: (() => void) | undefined;
 
   connectedCallback() {
@@ -213,48 +214,80 @@ export class VocablyFixGrammar {
           </div>
         </form>
 
-        {this.result && (
-          <Fragment>
-            {this.result.success === false && (
-              <div class="result result--error">{this.result.reason}</div>
-            )}
+        <div ref={(el) => (this.resultElement = el as HTMLDivElement)}>
+          {this.isLoading && (
+            <div class="result result--loading">
+              <vocably-skeleton-loader-bone
+                class="skeleton-loader-title"
+                style={{
+                  width: '100%',
+                  height: '32px',
+                  marginBottom: '8px',
+                }}
+              ></vocably-skeleton-loader-bone>
+              <vocably-skeleton-loader-bone
+                class="skeleton-loader-text"
+                style={{
+                  width: '85%',
+                }}
+              ></vocably-skeleton-loader-bone>
+              <vocably-skeleton-loader-bone
+                class="skeleton-loader-text"
+                style={{
+                  width: '83%',
+                }}
+              ></vocably-skeleton-loader-bone>
+              <vocably-skeleton-loader-bone
+                class="skeleton-loader-text"
+                style={{
+                  width: '87%',
+                }}
+              ></vocably-skeleton-loader-bone>
+            </div>
+          )}
+          {!this.isLoading && this.result && (
+            <Fragment>
+              {this.result.success === false && (
+                <div class="result result--error">{this.result.reason}</div>
+              )}
 
-            {this.result.success === true && (
-              <div class="result">
-                {this.result.value.isCorrect ? (
-                  <div class="correct-badge">
-                    <vocably-icon-check class="correct-icon" />
-                    No grammar issues found
-                  </div>
-                ) : (
-                  <div class="corrected-text">
-                    <div class="corrected-text__label">Corrected text</div>
-                    <div class="corrected-text__copy">
-                      <vocably-button-copy
-                        onCopy={() => {
-                          if (this.result?.success === true) {
-                            navigator.clipboard.writeText(
-                              this.result.value.text
-                            );
-                          }
-                        }}
-                      ></vocably-button-copy>
+              {this.result.success === true && (
+                <div class="result">
+                  {this.result.value.isCorrect ? (
+                    <div class="correct-badge">
+                      <vocably-icon-check class="correct-icon" />
+                      No grammar issues found
                     </div>
-                    <div class="corrected-text__value">
-                      {this.result.value.text}
+                  ) : (
+                    <div class="corrected-text">
+                      <div class="corrected-text__label">Corrected text</div>
+                      <div class="corrected-text__copy">
+                        <vocably-button-copy
+                          onCopy={() => {
+                            if (this.result?.success === true) {
+                              navigator.clipboard.writeText(
+                                this.result.value.text
+                              );
+                            }
+                          }}
+                        ></vocably-button-copy>
+                      </div>
+                      <div class="corrected-text__value">
+                        {this.result.value.text}
+                      </div>
                     </div>
-                  </div>
-                )}
-                <div
-                  class="explanation"
-                  innerHTML={mdConverter.makeHtml(
-                    this.result.value.explanation
                   )}
-                />
-              </div>
-            )}
-          </Fragment>
-        )}
+                  <div
+                    class="explanation"
+                    innerHTML={mdConverter.makeHtml(
+                      this.result.value.explanation
+                    )}
+                  />
+                </div>
+              )}
+            </Fragment>
+          )}
+        </div>
       </Host>
     );
   }
