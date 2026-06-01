51d15d8aaad468616c788cf768faac1dd8520bad
Dima Snisarenko <snisarenkodima@gmail.com>
2026-06-01 17:04:18 +0200

    feat(infra): api-e cname

--- platform/posthog.tf
+++ platform/posthog.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "posthog_reverse_proxy" {
   ttl     = 300
   records = ["3ec91268995519503ea6.cf-prod-us-proxy.proxyhog.com."]
 }
+
+resource "aws_route53_record" "posthog_reverse_proxy_eu" {
+  zone_id = data.aws_route53_zone.primary.id
+  name    = "api-e"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["543c520dfdd003a08aa4.cf-prod-eu-proxy.europehog.com."]
+}
