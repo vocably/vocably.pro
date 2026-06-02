3a19b920aa757492f2a94319aba37daee334d532
Dima Snisarenko <snisarenkodima@gmail.com>
2026-06-02 14:19:03 +0200

    feat(moa): sign in language

--- mobile-app/src/auth/logInFunctions.ts
+++ mobile-app/src/auth/logInFunctions.ts
@@ -1,11 +1,15 @@
 import { signInWithRedirect } from 'aws-amplify/auth';
 import { forcefulSignOut } from '../forcefulSignOut';
+import { i18n } from '../i18n';
 
 export const signIn = async () => {
+  const language = i18n.language;
+
   try {
     await signInWithRedirect({
       options: {
         prompt: 'LOGIN',
+        lang: { es: 'es', pt: 'pt-BR' }[language] ?? 'en',
       },
     });
   } catch (e) {
